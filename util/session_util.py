af7b07d8b2345a8c08c6098c6bfe3cee5b13aefb
Evgeny Shandrik <yauheni.shandryk@neotech.by>
2022-08-31 23:29:48 +0300

    add allure step

--- util/session_util.py
+++ util/session_util.py
@@ -3,6 +3,9 @@ Session util
 """
 import os
 
+import allure
+import curlify
+import logging
 from requests import Session
 
 
@@ -15,8 +18,25 @@ class BaseSession(Session):
         self.base_url = kwargs.pop('base_url')
         super().__init__()
 
+    @allure.step('{method} {url}')
     def request(self, method, url, **kwargs):
-        return super().request(method, url=f'{self.base_url}{url}', **kwargs)
+        response = super().request(method, url=f'{self.base_url}{url}', **kwargs)
+        message = curlify.to_curl(response.request)
+        logging.info(f'cURL: {message}\n')
+        logging.info(f'Response: {response.json()}\n')
+        allure.attach(
+            body=message.encode('utf8'),
+            name=f'Request {method}',
+            attachment_type=allure.attachment_type.TEXT,
+            extension='txt'
+        )
+        allure.attach(
+            body=str(response.json()).encode('utf-8'),
+            name='Response',
+            attachment_type=allure.attachment_type.TEXT,
+            extension='txt'
+        )
+        return response
 
 
 def req_session() -> BaseSession:
